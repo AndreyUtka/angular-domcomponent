407d7560d5eba9fd1469f16cb2440a8239b8ea4a
Andrey Michaluik <andreyutka@gmail.com>
2016-03-11 17:23:23 +0200

    add support for common js

--- src/angular-domcomponent.js
+++ src/angular-domcomponent.js
@@ -1,3 +1,8 @@
+
+if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
+    module.exports = 'angular-domcomponent';
+}
+
 (function() {
     var ng = angular.module;
     /**
